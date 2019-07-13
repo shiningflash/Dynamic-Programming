3ca00b01e862b74ea04b4e4615e2368cfd6d8de4
Amirul Islam <35567854+shiningflash@users.noreply.github.com>
2019-07-14 03:10:36 +0600

    Update longestPalindromeSubsequence.cpp

--- longestPalindromeSubsequence.cpp
+++ longestPalindromeSubsequence.cpp
@@ -39,8 +39,8 @@ lps sequence :: "ROTATOR"
 
 */
 
-// DP Solution :: TC - O(N^2)
-int lps(string s, int n) {
+// DP Solution :: TC - O(N^2) :: SC - O(N^2)
+int lps_2D(string s, int n) {
     int dp[n][n];
     for (int i = 0; i < n; i++) dp[i][i] = 1;
     for (int cell = 2; cell <= n; cell++) {
@@ -54,7 +54,29 @@ int lps(string s, int n) {
     return dp[0][n-1];
 }
 
+// Most efficient one *** ///////////////
+// DP Solution :: TC - O(N^2) :: SC - O(N)
+int lps_1D(string s, int n) {
+    int dp[n];
+    for (int i = n-1; i >= 0; i--) {
+        int back_up = 0;
+        for (int j = i; j < n; j++) {
+            if (i == j) dp[j] = 1;
+            else if (s[i] == s[j]) {
+                dp[j] = back_up + 2;
+                back_up = dp[j] - 2;
+            }
+            else {
+                back_up = dp[j];
+                dp[j] = max(dp[j-1], dp[j]);
+            }
+        }
+    }
+    return dp[n-1];
+}
+
 int main() {
     string s = "TURBOVENTILATOR";
-    cout << lps(s, s.size()) << endl;
+    //cout << lps_2D(s, s.size()) << endl;
+    cout << lps_1D(s, s.size()) << endl;
 }
